b6ee5d75113413811bf6f51ab6a4c1000b2b5dae
AlexMarchu <sasha.mar4enko2005@gmail.com>
2025-06-02 22:34:31 +1000

    add car number generation for users

--- users/fixtures/generate_users.py
+++ users/fixtures/generate_users.py
@@ -3,6 +3,7 @@ import string
 import random
 import os
 import sys
+import re
 
 import django
 from django.contrib.auth.hashers import make_password
@@ -22,6 +23,28 @@ def generate_password(length=12):
 def create_username(name):
     return ''.join(name.lower().split())
 
+def generate_car_number():
+    letters = 'АВЕКМНОРСТУХ'
+    
+    patterns = [
+        lambda: f"{random.choice(letters)}{random.randint(1, 999):03d}{random.choice(letters)}{random.choice(letters)}{random.randint(10, 999)}",
+        lambda: f"{random.choice(letters)}{random.choice(letters)}{random.randint(1, 9999):04d}{random.randint(10, 99)}",
+        lambda: f"{random.randint(1, 999):03d}{random.choice(letters)}{random.randint(1, 999):03d}{random.randint(10, 99)}",
+        lambda: f"Т{random.choice(letters)}{random.choice(letters)}{random.randint(1, 999):03d}{random.randint(10, 999)}",
+        lambda: f"{random.choice(letters)}{random.choice(letters)}{random.randint(1, 999):03d}{random.choice(letters)}{random.randint(10, 99)}",
+        lambda: f"{random.choice(letters)}{random.randint(1, 9999):04d}{random.randint(10, 99)}",
+        lambda: f"{random.randint(1, 999):03d}{random.choice(letters)}{random.randint(10, 99)}",
+        lambda: f"{random.randint(1, 9999):04d}{random.choice(letters)}{random.randint(10, 99)}",
+        lambda: f"{random.choice(letters)}{random.choice(letters)}{random.randint(1, 9999):04d}{random.randint(10, 99)}",
+        lambda: f"{random.choice(letters)}{random.choice(letters)}{random.randint(1, 999):03d}{random.randint(10, 999)}",
+        lambda: f"Т{random.choice(letters)}{random.choice(letters)}{random.randint(1, 999):03d}{random.randint(10, 999)}"
+    ]
+    
+    while True:
+        car_number = random.choice(patterns)()
+        if re.fullmatch(r'^(([АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{1,2})(\d{2,3})|(\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2})(\d{2})|(\d{3}(?<!000)(C?D|[ТНМВКЕ])\d{3}(?<!000))(\d{2}(?<!00))|([ТСК][АВЕКМНОРСТУХ]{2}\d{3}(?<!000))(\d{2})|([АВЕКМНОРСТУХ]{2}\d{3}(?<!000)[АВЕКМНОРСТУХ])(\d{2})|([АВЕКМНОРСТУХ]\d{4}(?<!0000))(\d{2})|(\d{3}(?<!000)[АВЕКМНОРСТУХ])(\d{2})|(\d{4}(?<!0000)[АВЕКМНОРСТУХ])(\d{2})|([АВЕКМНОРСТУХ]{2}\d{4}(?<!0000))(\d{2})|([АВЕКМНОРСТУХ]{2}\d{3}(?<!000))(\d{2,3})|(^Т[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}))$', car_number):
+            return car_number
+
 def import_data():
     path = os.path.join(os.path.dirname(__file__), 'loyalty_card.json')
 
@@ -34,21 +57,24 @@ def import_data():
     users_data = list()
 
     for card_data in data['LoyaltyCard']:
-        first_name, last_name = map(str.capitalize, card_data['holder_name'].split())
-        username = create_username(card_data['holder_name'])
-        password = generate_password()
-
         loyalty_card = LoyaltyCard.objects.create(
             number=card_data['number'],
             balance=card_data['balance']
         )
+
+        first_name, last_name = map(str.capitalize, card_data['holder_name'].split())
+        username = create_username(card_data['holder_name'])
+        password = generate_password()
+        car_number = generate_car_number()
+
 
         user = User.objects.create(
             username=username,
             password=make_password(password),
             first_name=first_name,
             last_name=last_name,
-            loyalty_card=loyalty_card
+            loyalty_card=loyalty_card,
+            car_number=car_number
         )
 
         users_data.append({
@@ -56,6 +82,7 @@ def import_data():
             'password': password,
             'first_name': first_name,
             'last_name': last_name,
+            'car_number': car_number,
             'loyalty_card_number': card_data['number']
         })
 
@@ -65,7 +92,7 @@ def import_data():
     with open(output_path, 'w', encoding='utf-8') as file:
         json.dump(users_data, file, ensure_ascii=False, indent=2)
 
-    print(f'Created {len(users_data)} users and loyalty_cards')
+    print(f'Created {len(users_data)} users with loyalty_cards and car_numbers')
 
 if __name__ == '__main__':
     import_data()
