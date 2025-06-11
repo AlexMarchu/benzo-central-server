fbb51ff49522e07692f9c75c7f3f601d38d22067
AlexMarchu <sasha.mar4enko2005@gmail.com>
2025-06-11 18:50:04 +1000

    test

--- backend/asgi.py
+++ backend/asgi.py
@@ -23,11 +23,11 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        # "websocket": AllowedHostsOriginValidator(
-        #     AuthMiddlewareStack(URLRouter(channels_app.routing.websocket_urlpatterns))
-        # ),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(channels_app.routing.websocket_urlpatterns)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(channels_app.routing.websocket_urlpatterns))
         ),
+        # "websocket": AuthMiddlewareStack(
+        #     URLRouter(channels_app.routing.websocket_urlpatterns)
+        # ),
     }
 )
